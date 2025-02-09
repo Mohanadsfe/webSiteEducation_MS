f3f7a7f76772d81ff15fdfa49058c74156b6e0c2
MustafaShama98 <mostfashmma@gmail.com>
2025-02-09 16:23:28 +0200

    height

--- src/components/GoogleCalendar.js
+++ src/components/GoogleCalendar.js
@@ -4,7 +4,7 @@ const GoogleCalendar = () => {
     return (
         <div>
             <h1>Google Calendar</h1>
-<iframe title="Google Calendar" src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ2IfX3hfRDLZdGCIUPpd-SrNRsdZ1tV9C3klsH4UPVFCgo4OLmXZ4Z3O4Ne-d4_tzWwoI5Cuzcv?gv=true" style={{ border: 0 }} width="100%" height="600" frameBorder="0"></iframe>
+<iframe title="Google Calendar" src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ2IfX3hfRDLZdGCIUPpd-SrNRsdZ1tV9C3klsH4UPVFCgo4OLmXZ4Z3O4Ne-d4_tzWwoI5Cuzcv?gv=true" style={{ border: 0 }} width="100%" height="675em" frameBorder="0"></iframe>
         </div>
     );
 };
